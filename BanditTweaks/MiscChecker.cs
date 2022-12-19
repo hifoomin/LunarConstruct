33b3aa52ac7f12d615d308356639367b26c09676
hifoomin <jakubrataj@protonmail.com>
2022-12-19 13:03:03 +0100

    first

---
+++ BanditTweaks/MiscChecker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HBT
+{
+    public abstract class MiscBase<T> : MiscBase where T : MiscBase<T>
+    {
+        public static T instance { get; set; }
+
+        public MiscBase()
+        {
+            if (instance != null)
+            {
+                throw new InvalidOperationException("Singleton class " + typeof(T).Name + " was instantiated twice");
+            }
+            instance = this as T;
+        }
+    }
+}
